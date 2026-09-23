d7c98e2378dc04e2269942f18685eb0fa8909203
Rodrigo Murillo <rodrimurillo08@gmail.com>
2026-09-23 10:14:01 -0500

    Calcular promedio

--- Program.cs
+++ Program.cs
@@ -10,3 +10,15 @@
 int[] notas = [15,18,12,17,20];
 
 ImprimirNotas(notas);
+static double CalcularPromedio(int[] notas)
+{
+    int suma=0;
+    for(int i = 0; i < notas.Length; i++)
+    {
+        suma+=notas[i];
+    }
+    return (double) suma / notas.Length;
+    
+}
+double promedio = CalcularPromedio(notas);
+System.Console.WriteLine($"El promedio es: {promedio}");
