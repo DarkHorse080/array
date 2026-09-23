d46d2e3fe7b5bb078645e2ddb3b9cbe8a9852871
Rodrigo Murillo <rodrimurillo08@gmail.com>
2026-09-23 09:14:14 -0500

    Impresión de notas función

--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
-﻿using System.Runtime.InteropServices;
-
-int[] notas = [15,18,12,17,20];
-for (int i=0; i < notas.Length; i++)
+﻿static void ImprimirNotas(int[] notas)
+{
+    for (int i=0; i < notas.Length; i++)
 {
     System.Console.WriteLine(notas[i]);
 }
+}
+
+
+int[] notas = [15,18,12,17,20];
+
+ImprimirNotas(notas);
